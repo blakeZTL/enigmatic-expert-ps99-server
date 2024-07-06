106883309a161db30ef0f2032eb341d2a5ef5aa0
Blake Bradford <james.b.bradford@faa.gov>
2024-07-06 10:25:13 -0400

    add is_during_active_battle

--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 import os
+import time
+import requests
 
 
 def clear_screen():
@@ -6,3 +8,26 @@ def clear_screen():
     command = "cls" if os.name == "nt" else "clear"
     # Execute command to clear the screen
     os.system(command)
+
+
+def is_during_active_battle():
+    url: str = "https://biggamesapi.io/api/activeClanBattle"
+    response = requests.get(url)
+    if not response.ok:
+        return False
+
+    res_data = response.json()
+    if "status" in res_data and res_data["status"] == "ok":
+        data = res_data["data"]
+        if "configData" not in data:
+            return False
+        config_data = data["configData"]
+        start_time: int = config_data["StartTime"]
+        end_time: int = config_data["FinishTime"]
+        current_time: int = int(time.time())
+        is_during_active_battle = (current_time > start_time) and (
+            current_time < end_time
+        )
+        print(f"Is during active battle: {is_during_active_battle}")
+        return is_during_active_battle
+    return False
